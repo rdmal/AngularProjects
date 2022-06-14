6ee2f64f4b2f89e760d9e0d9741775469589b55e
Raji Mallina <99835702+rdmal@users.noreply.github.com>
2022-06-14 16:50:05 -0400

    Add files via upload

---
+++ movie.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  movielist: movie[] = [
+    {moviename:'Mission Impossible Fallout', director:'jon woo', actor: 'Tom Cruise', year:'2018'},
+    {moviename:'Top Gun Mavern', director:'Joseph Kosinski', actor: 'Tom Cruise', year:'2022'},
+    {moviename:'La La Land', director:'Damien Chazelle', actor: 'Emma Stone', year:'2016'},
+    {moviename:'Doctor Strange', director:'Sam Raimi',actor: 'Elizabeth Olsen', year:'2022'},
+    {moviename:'Avengers:Endgame', director:'Anthony&Joe Russo', actor:'Robert Downey', year:'2019'}
+  ]
+}
+class movie{
+  moviename: string | undefined;
+  director: string | undefined;
+  actor: string | undefined;
+  year: string | undefined;
+
+}
